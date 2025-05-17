34488cddea461763039a85c9e3ccfc464084edc7
Ahmed <134275679+coder1467@users.noreply.github.com>
2025-05-17 19:34:11 +0500

    Update getWeather to return data

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 async function getWeather(location) {
     let response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=9AQNULLRWZR3VFTNNDGN94J2A`);
     response = await response.json();
-    console.log(response);
+    return response;
 }
-
-getWeather('London');
